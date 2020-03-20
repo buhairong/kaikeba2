249ee498ed3846cdbe13fe3e4e349fddd2b0a9e3
buhairong <22858111@qq.com>
2020-03-20 13:43:17 +0800

    36

---
+++ 高级/第十八章 typescript 录播/第八节课件/练习/dist/a.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var b_1 = require("./b");
+var a1 = 100;
+var a2 = 200;
+console.log(b_1.default);
+exports.default = {};
---
+++ 高级/第十八章 typescript 录播/第八节课件/练习/src/b.ts
@@ -0,0 +1,4 @@
+let a1 = 1000
+let a2 = 2000
+
+export default a1
