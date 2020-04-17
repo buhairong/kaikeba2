58125b8eb72745e8a96c260615890679877466b3
buhairong <22858111@qq.com>
2020-04-17 15:10:52 +0800

    50

---
+++ 高级/第二十四章小程序/3/小程序03/视图完成版本/my-project/src/components/navigationBar.vue
@@ -0,0 +1,25 @@
+<template>
+ <div>
+     <div class="mytitle">测试标题</div>
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+
+     }
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style scoped>
+    .mytitle{
+        color: red;
+    }
+    
+</style>
