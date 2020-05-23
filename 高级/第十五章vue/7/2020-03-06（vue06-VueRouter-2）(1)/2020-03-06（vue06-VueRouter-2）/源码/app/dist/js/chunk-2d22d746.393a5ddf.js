8f9022b8747205e83e8eacd57c7b87fa78f6a7ba
buhairong <22858111@qq.com>
2020-05-23 14:23:50 +0800

    del

--- 高级/第十五章vue/7/2020-03-06（vue06-VueRouter-2）(1)/2020-03-06（vue06-VueRouter-2）/源码/app/dist/js/chunk-2d22d746.393a5ddf.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" About ")])},c=[],o={name:"About",created:function(){}},a=o,l=t("2877"),r=Object(l["a"])(a,u,c,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d22d746.393a5ddf.js.map
